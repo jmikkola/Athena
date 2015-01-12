7bcf8705d951917b3b15a1ff801adb77529fae5a
Jeremy Mikkola <jeremy@jeremymikkola.com>
2015-01-11 20:23:06 -0800

    Making repls is awesome

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,6 @@
 import System.Console.Readline
 
+import SExpression
 import Parse
 import Read
 
@@ -7,7 +8,19 @@ main :: IO ()
 main = repl "> " tryParsing
 
 tryParsing :: String -> IO ()
-tryParsing line = putStrLn $ show $ parseSexpressions line
+tryParsing line = case parseSexpressions line of
+  Right parsed -> do
+    mapM handleExpr parsed
+    return ()
+  Left err -> print err
+
+handleExpr :: SExpression -> IO ()
+handleExpr sexpr = case readFunction sexpr of
+    Right fn -> print fn
+    Left err -> case readExpression sexpr of
+      Right expr -> print expr
+      Left err2 -> putStrLn $ unlines [
+        "Couldn't parse as either function or expression", err, err2]
 
 repl :: String -> (String -> IO ()) -> IO ()
 repl prompt fn = do
