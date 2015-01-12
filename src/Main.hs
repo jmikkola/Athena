a9bf978d4a18388ada7748610def1698e924bd5d
Jeremy Mikkola <jeremy@jeremymikkola.com>
2015-01-11 17:39:13 -0800

    More proper readline based repl

--- src/Main.hs
+++ src/Main.hs
@@ -10,11 +10,12 @@ tryParsing line = putStrLn $ show $ parseSexpressions line
 
 repl :: String -> (String -> IO ()) -> IO ()
 repl prompt fn = do
-  putStr prompt
-  line <- getLine
-  if line == "end"
-    then return ()
-    else do
-      fn line
-      repl prompt fn
-
+  maybeLine <- readline prompt
+  case maybeLine of
+    Nothing -> return ()
+    Just "end" -> return ()
+    Just "exit" -> return ()
+    Just line -> do
+                 addHistory line
+                 fn line
+                 repl prompt fn
