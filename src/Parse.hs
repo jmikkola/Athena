abeecccba57ec46f14d28cf062141f5a985d2688
Jeremy Mikkola <jeremy@jeremymikkola.com>
2015-06-20 18:06:34 -0700

    Add numeric literal parsing

--- src/Parse.hs
+++ src/Parse.hs
@@ -1,17 +1,33 @@
 module Parse where
 
-import Control.Applicative ((<$>), (<*>))
-
 import Text.Parsec
-import Text.Parsec.Char (char, digit, satisfy)
-import Text.Parsec.Expr
-import Text.Parsec.Language
-import Text.Parsec.Prim
-import Text.Parsec.String
-import Text.Parsec.Token
+import Text.Parsec.String (Parser)
 
 data LiteralValue = LiteralFloat Float | LiteralInt Int
                   deriving (Show, Eq)
+
+unwrapOr :: Maybe a -> a -> a
+unwrapOr (Just a) _ = a
+unwrapOr Nothing  b = b
+
+maybeEmpty :: Maybe String -> String
+maybeEmpty m = unwrapOr m ""
+
+literal :: Parser LiteralValue
+literal = choice [numericLiteral]
+
+numericLiteral :: Parser LiteralValue
+numericLiteral = do
+  start <- digits
+  choice [float start, integer start]
+
+integer :: String -> Parser LiteralValue
+integer ds = return $ LiteralInt (read ds)
+
+float :: String -> Parser LiteralValue
+float ds = do
+  fp <- floatingPart
+  return $ LiteralFloat (read (ds ++ fp))
 
 digits :: Parser String
 digits = do
@@ -24,6 +40,21 @@ _digit = do
   _ <- optional underscore
   digit
 
+floatingPart :: Parser String
+floatingPart = do
+  _ <- char '.'
+  ds <- optionMaybe digits
+  exPart <- optionMaybe exponentPart
+  return ('.' : (unwrapOr ds "0") ++ (maybeEmpty exPart))
+
+exponentPart :: Parser String
+exponentPart = do
+  _ <- char 'e'
+  sign <- choice [string "+", string "-", string ""]
+  ds <- many digit
+  return ('e' : sign ++ ds)
+
+whitespaceChs :: String
 whitespaceChs = " \t\r\n"
 
 otherChars :: Parser Char
--- test/ParseTest.hs
+++ test/ParseTest.hs
@@ -1,19 +1,21 @@
 module Main where
 
+import Control.Applicative ( (<*) )
+import Control.Monad (liftM)
+
 import Test.HUnit ( Test( TestList ), Assertion, (~=?), (@=?), (~:), assertBool)
 import Test.Framework (defaultMain, testGroup)
 import Test.Framework.Providers.HUnit (hUnitTestToTests)
 
-import Text.Parsec ( parse )
+import Text.Parsec ( parse, eof )
 import Text.Parsec.String ( Parser )
 
 import Parse
 
 
-main = defaultMain [digitTests]
-
-digitTests = testGroup "digits" (hUnitTestToTests testParseDigits)
-
+main = defaultMain $ asGroup [ ("digits", testParseDigits)
+                             , ("numeric literals", testNumericLiterals)
+                             ]
 
 asGroup namedTests = map convert namedTests
   where convert (name, fn) = testGroup name (hUnitTestToTests fn)
@@ -25,14 +27,12 @@ maybeEqEither _        _         = False
 
 assertParses :: (Eq a, Show a) => Parser a -> String -> Maybe a -> Assertion
 assertParses parser input expectation = assertBool message matched
-  where result    = parse parser "test" input
+  where result    = parse (parser <* eof) "test" input
         matched   = maybeEqEither expectation result
         resultStr = case expectation of
           Nothing -> "not parse"
           Just r  -> "parse to " ++ show r
-        message   = "expected " ++ input ++ " to " ++ resultStr
-
-oneEqualsTwo = "one = two" ~: 1 @=? 1
+        message   = "expected " ++ input ++ " to " ++ resultStr ++ ", got " ++ (show result)
 
 goodDigitsTestCases = [ ("1", "1")
                       , ("10", "10")
@@ -54,3 +54,14 @@ testGoodDigits = TestList $ map makeTest goodDigitsTestCases
 
 testBadDigits = TestList $ map makeTest badDigitsTestCases
   where makeTest input = input ~: assertParses digits input Nothing
+
+numericLiteralTestCases = [ ("1", Just (LiteralInt 1))
+                          , ("2_001_003", Just (LiteralInt 2001003))
+                          , ("1.", Just (LiteralFloat 1.0))
+                          , ("10_6.2_5", Just (LiteralFloat 106.25))
+                          , ("0.125e+2", Just (LiteralFloat 12.5))
+                          , ("1e5", Nothing)
+                          ]
+
+testNumericLiterals = TestList $ map makeTest numericLiteralTestCases
+  where makeTest (input, expectation) = input ~: assertParses numericLiteral input expectation
