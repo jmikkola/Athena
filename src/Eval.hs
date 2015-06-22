36fd8c319aa165d5f647809c8a1fa0cd5cc654bf
Jeremy Mikkola <jeremy@jeremymikkola.com>
2015-06-22 08:59:01 -0700

    Add very basic evaluation

--- src/Eval.hs
+++ src/Eval.hs
@@ -32,32 +32,76 @@ translate (ExpressionVar name)      = VarExpr name
 translate (ExpressionParen inner)   = translate inner
 translate (ExpressionFnCall _ _)    = error "TODO"
 translate (ExpressionBinary op l r) = BinaryExpr op (translate l) (translate r)
-translate (ExpressionUnary op expr) = UnaryExpr op (translate expr)
+translate (ExpressionUnary op ex)   = UnaryExpr op (translate ex)
 
 evalExpr :: EvalContext -> Expr -> Either EvalError Expr
-evalExpr ctx expr =
-  case expr of
-    e@(IntValExpr _)    -> Right e
-    e@(FloatValExpr _)  -> Right e
-    e@(StringValExpr _) -> Right e
-    e@(StructValExpr _) -> Right e
-    e@(VarExpr varname) ->
+evalExpr ctx ex =
+  case ex of
+    (IntValExpr _)      -> Right ex
+    (FloatValExpr _)    -> Right ex
+    (StringValExpr _)   -> Right ex
+    (StructValExpr _)   -> Right ex
+    (VarExpr varname)   ->
       case Map.lookup varname ctx of
         Just value -> Right value
-        Nothing    -> Left $ EvalError { message="Variable not found", expr=e }
-    e@(UnaryExpr op ex)  -> do
+        Nothing    -> Left $ EvalError { message="Variable not found", expr=ex }
+    (UnaryExpr op ex)   -> do
       inner <- evalExpr ctx ex
-      evalUnary ctx op inner
+      evalUnary op inner
+    (BinaryExpr op l r) -> do
+      left <- evalExpr ctx l
+      right <- evalExpr ctx r
+      evalBinary op left right
 
-evalUnary :: EvalContext -> UnaryOp -> Expr -> Either EvalError Expr
-evalUnary ctx op expr =
-  case expr of
-    e@(IntValExpr i)    ->
+evalUnary :: UnaryOp -> Expr -> Either EvalError Expr
+evalUnary op ex =
+  case ex of
+    (IntValExpr i)    ->
       case op of
         Negate -> Right . IntValExpr $ 0 - i
         Flip   -> Right . IntValExpr $ complement i
-    e@(FloatValExpr f)  -> Right e
-    e                   -> Left $ EvalError
+    (FloatValExpr f)  ->
+      case op of
+        Negate -> Right . FloatValExpr $ 0 - f
+        _      -> Left $ EvalError
+                        { message=("Can't apply unary op " ++ (display op) ++ " to a float")
+                        , expr=ex
+                        }
+    _                 -> Left $ EvalError
                                  { message=("Can't apply unary op " ++ (display op))
-                                 , expr=e
+                                 , expr=ex
                                  }
+
+evalBinary :: BinaryOp -> Expr -> Expr -> Either EvalError Expr
+evalBinary op l r =
+  case (l, r) of
+   (IntValExpr il, IntValExpr ir) ->
+     case op of
+      Plus   -> Right . IntValExpr $ il + ir
+      Minus  -> Right . IntValExpr $ il - ir
+      Times  -> Right . IntValExpr $ il * ir
+      Divide -> Right . IntValExpr $ il `div` ir
+      Mod    -> Right . IntValExpr $ il `mod` ir
+      Power  -> Right . IntValExpr $ il ^ ir
+   (FloatValExpr fl, FloatValExpr fr) ->
+     case op of
+      Plus   -> Right . FloatValExpr $ fl + fr
+      Minus  -> Right . FloatValExpr $ fl - fr
+      Times  -> Right . FloatValExpr $ fl * fr
+      Divide -> Right . FloatValExpr $ fl / fr
+      Mod    -> Left $ EvalError
+                      { message=("Can't apply mod to floats")
+                      , expr=(BinaryExpr op l r)
+                      }
+      Power  -> Right . FloatValExpr $ fl ** fr
+   (StringValExpr sl, StringValExpr sr) ->
+     case op of
+       Plus -> Right . StringValExpr $ sl ++ sr
+       _    -> Left $ EvalError
+                          { message=("Can't apply op " ++ (display op) ++ " to strings")
+                          , expr=(BinaryExpr op l r)
+                          }
+   _ -> Left $ EvalError
+               { message=("Can't apply binary op " ++ (display op) ++ " to " ++ (show l) ++ " and " ++ (show r))
+               , expr=(BinaryExpr op l r)
+               }
