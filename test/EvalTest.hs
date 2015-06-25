e1bfb7ffdc0a8666c070e9cc8551b447a60be6db
Jeremy Mikkola <jeremy@jeremymikkola.com>
2015-06-25 08:57:34 -0700

    Test evaluating structs

--- test/EvalTest.hs
+++ test/EvalTest.hs
@@ -24,15 +24,19 @@ testExpressions =
                              , ("unary -", UnaryExpr Negate (IntValExpr 123), Just $ IntValExpr (-123))
                              , ("unary ~", UnaryExpr Flip (IntValExpr 123), Just $ IntValExpr (-124))
                              , ("float ~", UnaryExpr Flip (FloatValExpr 123), Nothing)
-                             , ("float +",
-                                BinaryExpr Plus (FloatValExpr 5.0) (FloatValExpr 1.5),
-                                Just $ FloatValExpr 6.5)
-                             , ("integer %",
-                                BinaryExpr Mod (IntValExpr 123) (IntValExpr 10),
-                                Just $ IntValExpr 3)
-                             , ("mixed +",
-                                BinaryExpr Plus (IntValExpr 5) (FloatValExpr 1.5),
-                                Nothing)
+                             , ( "float +"
+                               , BinaryExpr Plus (FloatValExpr 5.0) (FloatValExpr 1.5)
+                               , Just $ FloatValExpr 6.5)
+                             , ( "integer %"
+                               , BinaryExpr Mod (IntValExpr 123) (IntValExpr 10)
+                               , Just $ IntValExpr 3)
+                             , ( "mixed +"
+                               , BinaryExpr Plus (IntValExpr 5) (FloatValExpr 1.5)
+                               , Nothing)
+                             , ( "struct expr"
+                               , StructValExpr "Pair" [ (BinaryExpr Plus (IntValExpr 100) (IntValExpr 42))
+                                                      , (VarExpr "anInt")]
+                               , Just $ StructValExpr "Pair" [(IntValExpr 142), (IntValExpr 123)])
                              ]
 
 testContext :: EvalContext
