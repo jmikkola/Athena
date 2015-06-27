5823ca521c7264a046aa6f5e519089ba37946474
Jeremy Mikkola <jeremy@jeremymikkola.com>
2015-06-27 10:22:29 -0700

    Evaluate statements

--- src/Main.hs
+++ src/Main.hs
@@ -10,22 +10,30 @@ import Eval
 import Parse
 
 main :: IO ()
-main = repl "expression> " tryParsing
+main = repl "expression> " tryParsing Map.empty
 
 trim :: String -> String
 trim = let revTrim = reverse . dropWhile isSpace
        in revTrim . revTrim
 
-tryParsing :: String -> IO ()
-tryParsing line = case parse (expression <* eof) "user input" line of
-  Right parsed -> handleParsed parsed
-  Left err -> print err
+tryParsing :: String -> EvalContext -> IO EvalContext
+tryParsing line ctx = case parse (statement <* eof) "user input" line of
+  Right parsed -> handleParsed parsed ctx
+  Left err -> do
+    print err
+    return ctx
 
-handleParsed :: Expression -> IO ()
-handleParsed = print . evalExpression Map.empty
+handleParsed :: Statement -> EvalContext -> IO EvalContext
+handleParsed stmt ctx = case evalStatement ctx stmt of
+  Left err -> do
+    print err
+    return ctx
+  Right (ctx', result) -> do
+    print result
+    return ctx'
 
-repl :: String -> (String -> IO ()) -> IO ()
-repl prompt fn = do
+repl :: String -> (String -> a -> IO a) -> a -> IO ()
+repl prompt fn state = do
   maybeLine <- readline prompt
   case maybeLine of
     Nothing -> return ()
@@ -34,5 +42,5 @@ repl prompt fn = do
     Just line ->
       do
         addHistory line
-        fn (trim line)
-        repl prompt fn
+        state' <- fn (trim line) state
+        repl prompt fn state'
