fc6cc806affba7420bc5bfa3f16a395ad18cbd79
Jeremy Mikkola <jeremy@jeremymikkola.com>
2016-01-16 13:22:54 -0800

    Start handling structs

--- src/InferExpression.hs
+++ src/InferExpression.hs
@@ -1,10 +1,6 @@
 module InferExpression where
 
-{-
-TODO:
-- Store a mapping of type constructor names to types
--}
-
+import Control.Monad (foldM)
 import Data.Map (Map)
 import qualified Data.Map as Map
 
@@ -49,22 +45,38 @@ data TIState = TIState { tirules  :: Rules
                        , tivargen :: VarGen
                        , tiscopes :: Scopes
                        , tireg    :: Registry
+                       , tictors  :: Map String String
                        }
              deriving (Show)
 
-startingState :: TIState
-startingState = TIState emptyRules [1..] emptyScope emptyRegistry
+-- TODO: also store the number of fields each type has
+-- TODO: figure out how to deal with generic types
+defaultCtors = Map.fromList
+               [ ("True", "Bool")
+               , ("False", "Bool")
+               , ("List", "List")
+               ]
 
-register :: (Typeable a) => a -> VarGen -> Registry -> (TypeVar, VarGen, Registry)
-register a vargen reg =
-  let (tvar:vargen') = vargen
-  in (tvar, vargen', Map.insert (asEntry a) tvar reg)
+startingState :: TIState
+startingState = TIState emptyRules [1..] emptyScope emptyRegistry defaultCtors
+
+register :: (Typeable a) => a -> TIState -> (TypeVar, TIState)
+register a tistate =
+  let (tvar:vargen) = tivargen tistate
+      registry = Map.insert (asEntry a) tvar (tireg tistate)
+  in (tvar, tistate { tivargen=vargen, tireg=registry })
 
 lookupVar :: String -> TIState -> ErrorS (TypeVar)
-lookupVar varName (TIState _ _ scope _) =
-  case Map.lookup varName (current scope) of
-    Nothing -> Left $ "Variable " ++ varName ++ " not found in scope " ++ (show scope)
+lookupVar varName tistate =
+  case Map.lookup varName (current $ tiscopes tistate) of
+    Nothing -> Left $ "Variable " ++ varName ++ " not found in scope " ++ (show $ tiscopes tistate)
     Just tv -> Right tv
+
+lookupCtor :: String -> TIState -> ErrorS (String)
+lookupCtor cfn tistate =
+  case Map.lookup cfn (tictors tistate) of
+    Nothing -> Left $ "Constructor " ++ cfn ++ " not found"
+    Just tp -> Right tp
 
 addRule :: (Rules -> Rules) -> TIState -> TIState
 addRule rule tistate = tistate { tirules = rule (tirules tistate) }
@@ -79,16 +91,16 @@ class Typeable a where
 instance Typeable LiteralValue where
   asEntry = LitEntry
 
-  ti (TIState rules vargen scopes reg) lit =
+  ti tistate lit =
     let typeName = case lit of
           LiteralFloat _  -> "Float"
           LiteralInt _    -> "Int"
           LiteralString _ -> "String"
           LiteralChar _   -> "Char"
         typeNode = TypeNode { constructor = typeName, components = [] }
-        (tvar, vargen', reg') = register lit vargen reg
-        rules' = specify tvar typeNode rules
-    in return (TIState rules' vargen' scopes reg', tvar)
+        (tvar, tistate') = register lit tistate
+        tistate'' = addRule (specify tvar typeNode) tistate'
+    in return (tistate'', tvar)
 
 instance Typeable Expression where
   asEntry = ExprEntry
@@ -103,10 +115,25 @@ instance Typeable Expression where
      (ExpressionFnCall fname exprs)      -> undefined
      (ExpressionBinary op left right)    -> undefined
      (ExpressionUnary op expr)           -> undefined
-     (ExpressionStruct tname exprs)      -> undefined
+     (ExpressionStruct tname exprs)      -> do
+       -- TODO: this might not correctly handle generic instantiation
+       let (tv, tistate1) = register expr tistate
+       structType <- lookupCtor tname tistate1
+       (tistate2, exprTypes) <- tiList tistate1 exprs
+       let sttype = TypeNode { constructor=structType, components=exprTypes }
+       let tistate3 = addRule (specify tv sttype) tistate2
+       return (tistate3, tv)
      (ExpressionIf test ifcase elsecase) -> do
        (tistate1, ttv) <- ti tistate test
        (tistate2, itv) <- ti tistate1 ifcase
        (tistate3, etv) <- ti tistate2 elsecase
        let tistate4 = addRule (specify ttv boolTN) $ addRule (setEqual itv etv) tistate3
        return (tistate4, itv)
+
+tiList :: (Typeable a) => TIState -> [a] -> ErrorS (TIState, [TypeVar])
+-- now here is where having `fmap` would be nice...
+tiList st xs = recur st xs []
+  where recur tist []     rest = Right (tist, reverse rest)
+        recur tist (x:xs) rest = do
+          (tist', xtv) <- ti tist x
+          recur tist' xs (xtv:rest)
