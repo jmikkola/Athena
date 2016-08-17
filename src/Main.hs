12f8b8ea2d5a35eba942400c109ce99ab667a111
Jeremy Mikkola <jeremy@jeremymikkola.com>
2016-08-16 21:48:39 -0700

    Start parser module

--- src/Main.hs
+++ src/Main.hs
@@ -2,8 +2,10 @@ module Main where
 
 import System.Environment ( getArgs )
 
+import Parser ( parse )
+
 main :: IO ()
 main = do
   args <- getArgs
   content <- readFile (args !! 0)
-  putStrLn content
+  putStrLn $ parse content
