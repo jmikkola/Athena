649435bb4d1b94b3bc337b81d64af42073ed721b
Jeremy Mikkola <jeremy@jeremymikkola.com>
2016-09-01 09:29:04 -0700

    Start emitter logic

--- src/Backends/Go/Emit.hs
+++ src/Backends/Go/Emit.hs
@@ -2,7 +2,16 @@ module Backends.Go.Emit where
 
 import Control.Monad.State
 
-import qualified Backends.Go.Syntax as Syntax
+--import qualified Backends.Go.Syntax as Syntax
+import Backends.Go.Syntax (
+  Type (..),
+  FunctionDecl (..),
+  NamedT (..),
+  Declaration (..),
+  Statement (..),
+  Expression (..),
+  UnaryOp (..),
+  BinaryOp (..) )
 
 data Output = Output { indent :: Int
                      , pieces :: [String]
@@ -34,3 +43,97 @@ writeIndent = do
 
 indentString :: Int -> String
 indentString n = replicate n '\t'
+
+--- Actual Emitters ---
+
+emitType :: Type -> EmitState ()
+emitType t = case t of
+  GoInt64
+    -> write "int64"
+  GoInt
+    -> write "int"
+  GoFloat64
+    -> write "float64"
+  GoBool
+    -> write "bool"
+  GoString
+    -> write "string"
+  GoChar
+    -> write "rune"
+  GoFunc args rets
+    -> do
+      write "func ("
+      intersperse (write ", ") (map emitType args)
+      write ")"
+      case rets of
+       []
+         -> return ()
+       [GoVoid]
+         -> return ()
+       [t']
+         -> do
+           write " "
+           emitType t'
+       _
+         -> do
+           write " ("
+           intersperse (write ", ") (map emitType rets)
+           write ")"
+  GoStruct name
+    -> do
+      write "*"
+      write name
+  GoInterface name
+    -> write name
+  GoVoid
+    -> return ()
+  TypeName name
+    -> write name
+
+writeReturn :: [NamedT] -> EmitState ()
+writeReturn []
+  = return ()
+writeReturn [JustType GoVoid]
+  = return ()
+writeReturn typs
+  = let inner = intersperse (write ", ") (map emitNamedT typs)
+    in if retNeedsParens typs
+       then do
+         write " ("
+         inner
+         write ")"
+       else do
+         write " "
+         inner
+         return ()
+
+retNeedsParens :: [NamedT] -> Bool
+retNeedsParens []
+  = False
+retNeedsParens [NamedType _ _]
+  = True
+retNeedsParens [_]
+  = False
+retNeedsParens _
+  = True
+
+emitNamedT :: NamedT -> EmitState ()
+emitNamedT (JustName name)
+  = write name
+emitNamedT (JustType t)
+  = emitType t
+emitNamedT (NamedType name t)
+  = do
+   write name
+   write " "
+   emitType t
+
+intersperse :: EmitState () -> [EmitState ()] -> EmitState ()
+intersperse sep lst =
+  case lst of
+   (a1:a2:rest) -> do
+     a1
+     sep
+     intersperse sep (a2:rest)
+   [a]          -> a
+   []           -> return ()
