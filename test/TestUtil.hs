77905ab2466e670f742ef74f1b189c1b7e82a1fd
Jeremy Mikkola <jeremy@jeremymikkola.com>
2016-09-04 11:19:34 -0700

    Remove old test directory

--- test/TestUtil.hs
+++
@@ -1,7 +0,0 @@
-module TestUtil where
-
-import Test.Framework (testGroup)
-import Test.Framework.Providers.HUnit (hUnitTestToTests)
-
-asGroup namedTests = map convert namedTests
-  where convert (name, fn) = testGroup name (hUnitTestToTests fn)
