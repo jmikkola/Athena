fb3bba323291a9b0352734219b72b04f1d118be3
Jeremy Mikkola <jeremy@jeremymikkola.com>
2016-09-08 21:47:45 -0700

    Fixed error handling

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,10 @@
 module Main where
 
 import Control.Monad (liftM)
---import Control.Monad.Error
+import Control.Monad.Except
 import System.Environment ( getArgs )
 import System.Process ( rawSystem )
-import System.Exit ( exitWith ) -- ExitCode (..)
+import System.Exit ( exitWith, ExitCode )
 
 import Parser ( parseFile )
 import ParserTest ( testMain )
@@ -15,21 +15,26 @@ import TypeCheck2 (runTypechecking)
 import Backends.Go.Convert (convertFile)
 import Backends.Go.Emit (emitFile)
 
-compile :: String -> IO (Either String Int)
+type ExitCodeResult = ExceptT String IO ExitCode
+
+either2Except :: Monad m => Either e a -> ExceptT e m a
+either2Except = ExceptT . return
+
+compile :: String -> ExitCodeResult
 compile content = do
-  parsed <- liftM $ parseFile content
-  checked <- liftM $ runTypechecking parsed
-  goSyntax <- liftM $ convertFile checked
-  output <- liftM $ emitFile goSyntax
-  writeFile "out.go" output
-  rawSystem "go" ["build", "-o", "a.out", "out.go"]
+  parsed <- either2Except $ parseFile content
+  checked <- either2Except $ runTypechecking parsed
+  goSyntax <- either2Except $ convertFile checked
+  output <- either2Except $ emitFile goSyntax
+  _ <- lift $ writeFile "out.go" output
+  lift $ rawSystem "go" ["build", "-o", "a.out", "out.go"]
 
 main :: IO ()
 main = do
   testMain
   args <- getArgs
   content <- readFile (args !! 0)
-  compileResult <- compile content
+  compileResult <- runExceptT $ compile content
   case compileResult of
    Left err ->
      putStrLn $ err
