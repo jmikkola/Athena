1b527b92b8e5499dd8634d775fa8e17f06ee6f93
Jeremy Mikkola <jeremy@jeremymikkola.com>
2017-02-21 21:50:08 -0800

    Fix coverage calculation

--- src/Matching.hs
+++ src/Matching.hs
@@ -12,6 +12,7 @@ import Type
 data Coverage
   = TotalCoverage
   | Partial (Map String Coverage)
+  | NoCoverage
   deriving (Eq, Show)
 
 data MatchError
@@ -27,13 +28,13 @@ type EnumOption = (String, [(String, TypeRef)])
 checkMatchExpressions :: TypeCheckState -> [IR.MatchExpression] -> Maybe MatchError
 checkMatchExpressions types exprs = case checkExpressions types exprs of
   Left  err           -> Just err
-  Right (Partial _)   -> Just Incomplete
   Right TotalCoverage -> Nothing
+  Right _             -> Just Incomplete
 
 checkExpressions :: TypeCheckState -> [IR.MatchExpression] -> MatchResult Coverage
 checkExpressions types exprs = do
   _ <- mapM (\e -> evalStateT (duplicatesCheck e) Set.empty) exprs
-  foldM (addCoverage types) noCoverage exprs
+  foldM (addCoverage types) NoCoverage exprs
 
 type VarUseState = StateT (Set String) MatchResult
 
@@ -54,9 +55,6 @@ addVar v = do
     then lift . Left . DuplicateName $ "variable name used twice in the same match case: " ++ v
     else put $ Set.insert v existing
 
-noCoverage :: Coverage
-noCoverage = Partial Map.empty
-
 unreachable :: String -> MatchResult a
 unreachable = Left . Unreachable
 
@@ -69,7 +67,7 @@ mustAddCoverage :: Coverage -> Coverage -> MatchResult Coverage
 mustAddCoverage existing new =
   let merged = mergeCoverage existing new
   in if merged == existing
-     then unreachable $ show new
+     then unreachable $ show "didn't add any coverage"
      else return merged
 
 genTree :: TypeCheckState -> IR.MatchExpression -> MatchResult Coverage
@@ -93,6 +91,7 @@ genTree types expr = case expr of
 foldTotals :: Coverage -> Coverage
 foldTotals coverage = case coverage of
   TotalCoverage  -> TotalCoverage
+  NoCoverage     -> NoCoverage
   Partial fields ->
     let foldedFields = Map.map foldTotals fields
     in if allTotal foldedFields then TotalCoverage else Partial foldedFields
@@ -106,7 +105,6 @@ getStructFieldNames tcs typ = case Map.lookup typ (types tcs) of
   Just t  -> case t of
     Struct fields -> return $ map fst fields
     _             -> fail "invalid type in getStructFieldNames"
-
 
 getEnumFromOption :: TypeCheckState -> TypeRef -> MatchResult TypeRef
 getEnumFromOption tcs typ = case Map.lookup typ (enumParents tcs) of
@@ -132,16 +130,18 @@ partialForVariant enumName fields =
   partialOf enumName $ merge $ map partialForField $ map fst fields
 
 partialForField :: String -> Coverage
-partialForField name = partialOf name noCoverage
+partialForField name = partialOf name NoCoverage
 
 partialOf :: String -> Coverage -> Coverage
 partialOf s c = Partial $ Map.singleton s c
 
 merge :: [Coverage] -> Coverage
-merge = foldl mergeCoverage noCoverage
+merge = foldl mergeCoverage NoCoverage
 
 mergeCoverage :: Coverage -> Coverage -> Coverage
-mergeCoverage _ TotalCoverage = TotalCoverage
-mergeCoverage TotalCoverage _ = TotalCoverage
-mergeCoverage (Partial m1) (Partial m2) =
+mergeCoverage _             TotalCoverage = TotalCoverage
+mergeCoverage TotalCoverage _             = TotalCoverage
+mergeCoverage NoCoverage    c             = c
+mergeCoverage c             NoCoverage    = c
+mergeCoverage (Partial m1)  (Partial m2)  =
   foldTotals $ Partial $ Map.unionWith mergeCoverage m1 m2
