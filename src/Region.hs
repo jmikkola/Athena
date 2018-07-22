94dc897efc4d9a2fa22127feaa57ab214a4694ab
Jeremy Mikkola <jeremy@jeremymikkola.com>
2018-07-22 09:52:19 -0700

    Add region module

---
+++ src/Region.hs
@@ -0,0 +1,14 @@
+module Region where
+
+data Region =
+  Region
+  { sourceName :: String
+  , start :: !Position
+  , end :: !Position
+  } deriving (Eq, Ord, Show)
+
+data Position =
+  Position
+  { line :: !Int
+  , column :: !Int
+  } deriving (Eq, Ord, Show)
