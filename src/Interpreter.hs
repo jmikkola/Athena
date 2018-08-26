98f6ba8193f5a0e873cf348715866916b46e2064
Jeremy Mikkola <jeremy@jeremymikkola.com>
2018-08-26 15:14:19 -0700

    Building type level machinery

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -1,7 +1,67 @@
-module Interpreter where
+{-# LANGUAGE DeriveFunctor #-}
+
+module Interpreter (interpret) where
+
+
+import System.IO (hFlush, stdout)
+
+import Control.Applicative ( (<|>) )
+import Control.Monad.Free (Free (..))
+
+import Data.Map (Map)
+import qualified Data.Map as Map
 
 import Inference
   ( InferResult(..) )
+import qualified AST.Expression as E
+import qualified AST.Statement as S
+import Types (Type(..), tUnit)
 
-interpret :: InferResult a -> IO ()
-interpret = undefined
+
+interpret :: InferResult () -> IO ()
+interpret body =
+  let mainT = TFunc [] tUnit
+      callMain = E.Call (tUnit, ()) (E.Var (mainT, ()) "main") []
+  in do
+    _ <- runOp (interpretExpr body callMain) startingState
+    return ()
+
+
+startingState :: Scope
+startingState = [Map.empty]
+
+runOp :: Free IOOp Value -> Scope -> IO Value
+runOp = undefined
+
+interpretExpr :: InferResult () -> E.Expression AnnT -> Free IOOp Value
+interpretExpr = undefined
+
+
+data IOOp next
+  = ExitSuccess
+  | ExitError
+  | ReadLine (String -> next)
+  | WriteLine String next
+  | GetVar String (Value -> next)
+  | PutVar String Value next
+  | Error String
+  deriving (Functor)
+
+data Value
+  = VInt Int
+  | VFloat Float
+  | VString String
+  | VBool Bool
+  | VList [Value]
+  | VClosure Value Function
+  | VVoid
+  deriving (Show)
+
+data Function
+  = Function [String] (S.Statement AnnT)
+  deriving (Show)
+
+type Scope = [Map String Value]
+
+-- AnnT is short for "Annotation Type"
+type AnnT = (Type, ())
