f2ad6313462774b9eba383c5a07feb9f3badb055
Jeremy Mikkola <jeremy@jeremymikkola.com>
2018-09-30 14:49:52 -0700

    Add more operations

--- tools/gencode.py
+++ tools/gencode.py
@@ -13,10 +13,12 @@ def gen_expr(target, prev_operator=None, depth=1):
         return literal_expr(target, depth)
     options = [
         literal_expr,
-        plus_expr,
-        minus_expr,
+        times_expr,
+        divide_expr,
         paren_expr,
     ]
+    if prev_operator not in ('*', '/'):
+        options += [minus_expr, plus_expr]
     option = random.choice(options)
     return option(target, depth+1)
 
@@ -32,12 +34,39 @@ def plus_expr(target, depth):
 def minus_expr(target, depth):
     rhs = random.randint(-1000, 1000)
     left = gen_expr(target+rhs, '-', depth)
-    right = gen_expr(rhs, '-', depth)
+    right = literal_expr(rhs, depth)
     return left + ' - ' + right
+
+def times_expr(target, depth):
+    factor = random.choice(factorize(target))
+    left = gen_expr(target//factor, '*', depth)
+    right = literal_expr(factor, depth)
+    return left + ' * ' + right
+
+def divide_expr(target, depth):
+    multiplicand = random.randint(1, 100)
+    left = gen_expr(target*multiplicand, '/', depth)
+    right = literal_expr(multiplicand, depth)
+    return left + r' / ' + right
 
 def paren_expr(target, depth):
     inner = gen_expr(target, None, depth)
     return '(' + inner + ')'
+
+def factorize(n):
+    factors = []
+    factor = 1
+    while True:
+        if n % factor == 0:
+            factors.append(factor)
+            k = n / factor
+            if k != factor:
+                factors.append(factor)
+        factor += 1
+        if factor*factor > n:
+            break
+    return factors
+
 
 def main():
     expr = gen_expr(target=42)
